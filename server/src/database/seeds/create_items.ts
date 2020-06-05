84c4c3f5a0a213439ffbb187f1e94e2c88d2c0ed
JairoDoni <jairo.doni97@gmail.com>
2020-06-05 16:27:50 -0300

    Front-end com rotas, cadastro e consumindo a api

--- server/src/database/seeds/create_items.ts
+++ server/src/database/seeds/create_items.ts
@@ -3,8 +3,8 @@ import Knex from'knex';
 export async function seed(knex: Knex){
   await knex('items').insert([
     {title:'Lâmpadas', image:'lampadas.svg'},
-    {title:'Pilhas e Baterias', image:'lampadas.svg'},
-    {title:'Papéis e Papelão', image:'papeis_papelao.svg'},
+    {title:'Pilhas e Baterias', image:'baterias.svg'},
+    {title:'Papéis e Papelão', image:'papeis-papelao.svg'},
     {title:'Residuos Eletronicos', image:'eletronicos.svg'},
     {title:'Residuos Organicos', image:'organicos.svg'},
     {title:'Óleo de Cozinha', image:'oleo.svg'},
